e89cb3f16837d3ca72acfb68ffe4940c3f651db8
Simone Acuti <acutisimone@gmail.com>
2023-10-22 23:21:43 +0200

    Create m/m/c model

--- mmc/admin.py
+++ mmc/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Mmc
 
 # Register your models here.
+admin.site.register(Mmc)
--- mmc/models.py
+++ mmc/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Mmc(models.Model):
+    myLambda = models.FloatField()
+    mu = models.FloatField()
+    c = models.IntegerField()
+
+    def __str__(self):
+        return "Lambda: " + str(self.myLambda) + " Mu: " + str(self.mu)
