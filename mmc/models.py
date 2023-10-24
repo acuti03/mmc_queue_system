af5e333b05353cef8f72c6750cd0a06e2535df4b
Simone Acuti <acutisimone@gmail.com>
2023-10-25 01:49:01 +0200

    Update models

--- mmc/models.py
+++ mmc/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Mmc(models.Model):
     myLambda = models.FloatField()
     mu = models.FloatField()
-    c = models.IntegerField()
+    c = models.FloatField()
 
     def __str__(self):
         return "Lambda: " + str(self.myLambda) + " Mu: " + str(self.mu) + " C: " + str(self.c)
