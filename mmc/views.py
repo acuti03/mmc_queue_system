3fbd2e1aec349dd985cf786f2cb1652c1036d334
Simone Acuti <acutisimone@gmail.com>
2023-10-24 14:03:11 +0200

    Add parameters control

--- mmc/views.py
+++ mmc/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render
 from .models import Mmc
+from json import dumps
 
 # Create your views here.
 def home(request):
 	c = 0;
+	myLambda = 0;
+	mu = 0;
 
 	if request.method == 'POST':
 		myLambda = request.POST['myLambda']
 		mu = request.POST['mu']
 		c = request.POST['c']
-	
+
+		if myLambda == '' or mu == '' or c == '':
+			myLambda = 0
+			mu = 0
+			c = 0
+		
 		newMmc = Mmc(myLambda = myLambda, mu = mu, c = c)
 		newMmc.save()
 
 	c = int(c);
-	context = { 
+	context = {
+		"c": c,
 		"data" : [n for n in range(0,c)], 
 	} 
+
+	dataJSON = dumps(context)
 
 	return render(request, 'mmc/index.html', context)
