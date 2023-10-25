bd240f3df69d339fc897605bdfa8680e38eab006
Simone Acuti <acutisimone@gmail.com>
2023-10-26 01:46:55 +0200

    Add mu_k field

--- mmc/static/script.js
+++ mmc/static/script.js
@@ -1,41 +1,53 @@
 class Main {
-    k;
+    k
     kField
+    mu_k
+    mu_kField
     myInterval
 
-    constructor(k){
+    constructor(k, mu_k){
         this.k = k;
-        this.kField = NaN
+        this.kField = NaN;
+        this.mu_k = mu_k;
+        this.mu_kField = NaN;
         this.myInterval = NaN;
     }
 
     printResult(){
         let i = 0;
+
         this.kField = document.getElementById('k');
+        this.mu_kField = document.getElementById('mu_k');
+
         document.getElementById('print').disabled = true;
         console.log(this.kField);
 
         this.myInterval = setInterval(() => {
 
-            if(i == this.k + 100){
+            if(i == this.k){
                 clearInterval(this.myInterval);
                 document.getElementById('print').disabled = false;
             }
 
             this.kField.innerHTML = i;
-            console.log(i);
+            this.mu_kField.innerHTML = mu_k[i];
+            console.log(mu_k[i]);
             i++;
-        }, 1000)
+        }, 500)
     }
 
     clearContent(){
         document.getElementById('print').disabled = false;
         this.kField.innerHTML = '';
+        this.mu_kField.innerHTML = '';
         clearInterval(this.myInterval);
     }
 }
 
 const data = document.currentScript.dataset;
-const k = parseInt(data.c);
-console.log(k);
-let main = new Main(k);
+const k = parseInt(data.k);
+const mu_k = JSON.parse(data.mu_k);
+
+console.log(k)
+console.log(mu_k);
+let main = new Main(k, mu_k);
--- mmc/views.py
+++ mmc/views.py
@@ -7,11 +7,12 @@ def home(request):
 	c = 0
 	myLambda = 0
 	mu = 0
+	mu_k = 0
 
 	if request.method == 'POST':
-		myLambda = request.POST['myLambda']
-		mu = request.POST['mu']
-		c = request.POST['c']
+		myLambda = float(request.POST['myLambda'])
+		mu = float(request.POST['mu'])
+		c = int(request.POST['c'])
 
 		if myLambda == '' or mu == '' or c == '':
 			myLambda = 0
@@ -20,9 +21,15 @@ def home(request):
 		
 		newMmc = Mmc(myLambda = myLambda, mu = mu, c = c)
 		newMmc.save()
+		
+		x = lambda k, c, mu : k * mu if (k * mu) < (c * mu) else c * mu
+		mu_k = dumps([x(k, c, mu) for k in range(0, c + 10)])
+		k = len([i for i in range(0, c + 10)]) - 1
 
 	context = {
 		"c": c,
+		"mu_k": mu_k,
+		"k": k
 	}
 
 	return render(request, 'mmc/index.html', context)
