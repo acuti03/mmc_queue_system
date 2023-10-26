f9eec7bc404748917f97978936d2d6661e1a58ea
Simone Acuti <acutisimone@gmail.com>
2023-10-27 01:49:26 +0200

    Add P_0 and P_k

--- mmc/static/script.js
+++ mmc/static/script.js
@@ -5,15 +5,23 @@ class Main {
     mu_kField
     rho
     rhoField
+    p_0
+    p_0Field
+    p_k
+    p_kField
     myInterval
 
-    constructor(k, mu_k, rho){
+    constructor(k, mu_k, rho, p_0, p_k){
         this.k = k;
         this.kField = NaN;
         this.mu_k = mu_k;
         this.mu_kField = NaN;
         this.rho = rho;
         this.rhoField = NaN;
+        this.p_0 = p_0;
+        this.p_k = p_k;
+        this.p_kField = NaN
+        this.p_0Field = NaN;
         this.myInterval = NaN;
     }
 
@@ -23,6 +31,8 @@ class Main {
         this.kField = document.getElementById('k');
         this.mu_kField = document.getElementById('mu_k');
         this.rhoField = document.getElementById('rho');
+        this.p_0Field = document.getElementById('p_0');
+        this.p_kField = document.getElementById('p_k')
 
         document.getElementById('print').disabled = true;
         console.log(this.kField);
@@ -35,8 +45,10 @@ class Main {
             }
 
             this.kField.innerHTML = i;
-            this.mu_kField.innerHTML = mu_k[i];
+            this.mu_kField.innerHTML = this.mu_k[i];
             this.rhoField.innerHTML = this.rho;
+            this.p_0Field.innerHTML = this.p_0;
+            this.p_kField.innerHTML = this.p_k[i];
             console.log(mu_k[i]);
             i++;
         }, 500)
@@ -47,6 +59,8 @@ class Main {
         this.kField.innerHTML = '';
         this.mu_kField.innerHTML = '';
         this.rhoField.innerHTML = '';
+        this.p_0Field.innerHTML = '';
+        this.p_kField.innerHTML = '';
         clearInterval(this.myInterval);
     }
 }
@@ -54,8 +68,12 @@ class Main {
 const data = document.currentScript.dataset;
 const k = parseInt(data.k);
 const mu_k = JSON.parse(data.mu_k);
-const rho = parseFloat(data.rho)
+const rho = parseFloat(data.rho);
+const p_0 = parseFloat(data.p_0);
+const p_k = JSON.parse(data.p_k)
 
-console.log(k)
-console.log(mu_k);
-let main = new Main(k, mu_k, rho);
+console.log("k: ", k)
+console.log("mu_k: ", mu_k);
+console.log("p_0: ", p_0);
+console.log("p_k", p_k)
+let main = new Main(k, mu_k, rho, p_0, p_k);
--- mmc/views.py
+++ mmc/views.py
@@ -8,34 +8,53 @@ def home(request):
 	c = 0
 	myLambda = 0
 	mu = 0
-	mu_k = 0
-	k = 0
+	mu_k = [0]
+	k = [0]
 	rho = 0
+	p_0 = 0
+	p_k = [0]
 
 	if request.method == 'POST':
 		myLambda = float(request.POST['myLambda'])
 		mu = float(request.POST['mu'])
 		c = int(request.POST['c'])
 
-		x = lambda a : a if a > 0 else (1 if a == 0 else -a)
+		x = lambda a : a if a > 0 else -a
 
 		myLambda = x(myLambda)
 		mu = x(mu)
 		c = x(c)
 		
+		x = lambda k, c, mu : "{:.2f}".format(k * mu) if (k * mu) < (c * mu) else "{:.2f}".format(c * mu)
+
+		mu_k = ([x(k, c, mu) for k in range(0, c + 10)])
+		k = len([i for i in range(0, c + 10)]) - 1
+		rho = float("{:.2f}".format(myLambda/(c * mu)))
+
+		for i in range(0, c):
+			p_0 += ((c * rho) ** i) / math.factorial(i) + (((c * rho) ** c) / math.factorial(c)) * (1/(1 - rho))
+		p_0 = float("{:.2f}".format(p_0 ** -1))
+
+		p_k = [i for i in range(0, k + 1)]
+		for k in p_k:
+			if k <= c:
+				p_k[k] = float("{:.2f}".format(p_0 * ((c * rho) ** k) / math.factorial(k)))
+			else:
+				p_k[k] = float("{:.2f}".format(p_0 * ((rho ** k) * (c ** c)) / math.factorial(c)))
+
+
+		print(k)
 		newMmc = Mmc(myLambda = myLambda, mu = mu, c = c)
 		newMmc.save()
-		
-		x = lambda k, c, mu : "{:.2f}".format(k * mu) if (k * mu) < (c * mu) else "{:.2f}".format(c * mu)
-		mu_k = dumps([x(k, c, mu) for k in range(0, c + 10)])
-		k = len([i for i in range(0, c + 10)]) - 1
-		rho = "{:.2f}".format(myLambda/(c * mu))
+
 
 	context = {
 		"c": c,
-		"mu_k": mu_k,
+		"mu_k": dumps(mu_k),
 		"k": k,
-		"rho": rho
+		"rho": rho,
+		"p_0": p_0,
+		"p_k": dumps(p_k)
 	}
 
 	return render(request, 'mmc/index.html', context)
