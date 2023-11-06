1cb756bbb2e50ca4235e91eb18ccdd6f5aac8390
Simone Acuti <acutisimone@gmail.com>
2023-11-06 03:02:49 +0100

    Add other parameters to models

--- mmc/models.py
+++ mmc/models.py
@@ -3,11 +3,20 @@ from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Mmc(models.Model):
-    myLambda = models.FloatField()
-    mu = models.FloatField()
-    c = models.FloatField()
-    k = models.IntegerField()
-    p_k = ArrayField(models.FloatField())
+	myLambda = models.FloatField()
+	mu = models.FloatField()
+	c = models.FloatField()
+	k = models.IntegerField()
+	mu_k = ArrayField(models.FloatField())
+	rho = models.FloatField()
+	p_0 = models.FloatField()
+	p_k = ArrayField(models.FloatField())
+	p_queue = models.FloatField()
+	l_q = models.FloatField()
+	l_x = models.FloatField()
+	l_s = models.FloatField()
+	w_q = models.FloatField()
+	w_s = models.FloatField()
 
-    def __str__(self):
-        return "Lambda: " + str(self.myLambda) + " Mu: " + str(self.mu) + " C: " + str(self.c)
+	def __str__(self):
+		return "Lambda: " + str(self.myLambda) + " Mu: " + str(self.mu) + " C: " + str(self.c)
